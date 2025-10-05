15cfefed289d8b6b6f0590e5f73d2ac3ea459653
Craskii <67607188+Craskii@users.noreply.github.com>
2025-10-05 18:35:21 -0400

    added service worker to fix white screen

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,16 @@
-const CACHE = 'kava-v2';
+const CACHE = 'kava-v2'; // bump this version if you change things
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/', '/manifest.json'])));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(['/', '/manifest.json']))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => self.clients.claim());
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
